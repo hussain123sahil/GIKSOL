ca353d782c572ad693d913edfb9df3090a1fe835
vivekPrasad <vivekprasad122@gmail.com>
2025-09-22 23:09:13 +0530

    siderbar-studentdashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,7 +10,7 @@ import { MentorDashboardComponent } from './components/mentor-dashboard/mentor-d
 import { AdminComponent } from './components/admin/admin';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: StudentDashboardComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
